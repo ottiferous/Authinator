5b6dd9a341d96146c7b73aad7f3be9d18fa608de
Andrew <github@andrewmarrone.com>
2017-11-01 15:53:19 -0700

    cleaning up helper functions

--- authinator.py
+++ authinator.py
@@ -34,36 +34,40 @@ auth_api = duo_client.Auth(
 
 # method to cleanly display auth methods available for a username
 # expects JSON['devices']
-def display_auth_options(devices):
+def display_auth_devices(devices):
     x = 0
-    auth_choices = []
     for _ in devices:
-        auth_choices.append( (x, _['device']) )
         if 'display_name' in _:
             print(str(x) + ". " + _['display_name'])
         else:
             print(str(x) + ". " + _['type'] + " " + _['name'])
         x += 1
-    return ( auth_choices, x )
+    return true
 
-def select_auth_method(uname, options):
-    state = ''
-    while state != 'q':
-        try:
-            state = input("Authenticate with device: ")
-            print("Sending push...")
-            auth_api.auth(username=uname, factor="auto", device=options[int(state)][int(state)][1])
-            print("Success!")
-            return true
-        except:
-            if state != 'q':
-                print("\nThat is not a valid option. Please choose a number from the list")
-                display_auth_options(answer['devices'])
+def display_device_capabiltiies(device):
+    x = 0
+    for _ in device['capabilities']:
+        print(str(x) + ". " + _)
+        x += 1
+    return true
+
+def select_auth_method(uname, devices):
+    try:
+        response = int(input("Authenticate with device # "))
+        display_device_capabiltiies(devices[response])
+    except:
+        print("That is not a valid option.")
 
 def auth_with_push(uname, device):
     try:
         print("Sending push to device...")
-        auth_api.auth(username=uname, factor="push", type="Identification", display_username="Help Desk")
+        auth_api.auth(
+            username=uname,
+            device=device,
+            factor="push",
+            type="Identification",
+            display_username="Help Desk"
+        )
         print("Success!")
     except Exception as e:
         print("Ooops! Something went wrong.")
@@ -74,7 +78,11 @@ def auth_with_passcode(uname, device):
     while state != 'q':
         try:
             state = input("6-digit code: ")
-            auth_api.auth(username=uname, factor="passcode", passcode=state)
+            auth_api.auth(
+                username=uname,
+                factor="passcode",
+                passcode=state
+            )
             print("Success!")
             state = 'q'
         except Exception as e:
@@ -83,15 +91,15 @@ def auth_with_passcode(uname, device):
                 print("\n that is not a valid passcode.")
 
 def select_user():
-    state = ''
-    while state != 'q':
+    response = ''
+    while response != 'q':
         try:
-            state = input("Type the user's name in Duo: ")
-            response = auth_api.preauth(username=state)
-            return (state, response)
+            response = input("Type the user's name in Duo: ")
+            devices = auth_api.preauth(username=state)
+            return (response, devices)
         except:
-            if state != 'q':
-                print("\n Username \"" + state + "\" was not found.")
+            if response != 'q':
+                print("\n Username \"" + response + "\" was not found.")
 
 
 
@@ -103,16 +111,16 @@ options = display_auth_options(auth_options['devices'])
 #auth_with_passcode(uname, "DPDMY4LY3MSY87B6UZ6")
 
 # program termination
-exit()
+#exit()
 
 # Please use your valid telephone number with country code, area
 # code, and 7 digit number. For example: PHONE = '+1-313-555-5555'
-PHONE_NUMBER = get_next_arg('phone number ("+1-313-555-5555"): ')
+#PHONE_NUMBER = get_next_arg('phone number ("+1-313-555-5555"): ')
 
-(pin, txid) = verify_api.call(phone=PHONE_NUMBER)
-print('Sent PIN: %s' % pin)
-state = ''
-while state != 'ended':
-    status_res = verify_api.status(txid=txid)
-    print(status_res['event'], 'event:', status_res['info'])
-    state = status_res['state']
+#(pin, txid) = verify_api.call(phone=PHONE_NUMBER)
+#print('Sent PIN: %s' % pin)
+#state = ''
+#while state != 'ended':
+    #status_res = verify_api.status(txid=txid)
+    #print(status_res['event'], 'event:', status_res['info'])
+    #state = status_res['state']
